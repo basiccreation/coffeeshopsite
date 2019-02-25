02d6146e7f8f131bc273c9fb687a1976564a1ccb
pia smith <basiccreation@yahoo.com>
2019-02-25 01:24:47 -0800

    changed case on info to Info

---
+++ src/components/Layout.js
@@ -0,0 +1,20 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+import "./bootstrap.min.css"
+import "./layout.css"
+import Navbar from "./globals/Navbar"
+import Footer from "./globals/Footer"
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+    {children}
+    <Footer />
+  </>
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
